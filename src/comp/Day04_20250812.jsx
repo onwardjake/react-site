9f65dfc56a6b6186aac70a779855a1571518ae39
onwardjake <onwardjake@gmail.com>
2025-08-12 15:45:20 +0900

    day04 20250812

---
+++ src/comp/Day04_20250812.jsx
@@ -0,0 +1,46 @@
+import Counter from './Day04_20250812/Counter';
+import ColorChanger from './Day04_20250812/ColorChanger';
+import ToggleButton from './Day04_20250812/ToggleButton';
+import TodoList from './Day04_20250812/TodoList';
+import ExUseEffect01 from './Day04_20250812/useEffect/ExUseEffect01';
+import ExUseEffect02 from './Day04_20250812/useEffect/ExUseEffect02';
+import ExUseEffect03 from './Day04_20250812/useEffect/ExUseEffect03';
+import ExUseEffect04 from './Day04_20250812/useEffect/ExUseEffect04';
+import ExUseEffect05 from './Day04_20250812/useEffect/ExUseEffect05';
+import MyForm from './Day04_20250812/MyForm';
+
+export default function Day04_20250812() {
+	return (
+		<>
+			<h2>React Day 04 20250812</h2>
+			<Counter />
+			<hr />
+			<p></p>
+			<ColorChanger />
+			<hr />
+			<p></p>
+			<ToggleButton />
+			<hr />
+			<p></p>
+			<TodoList />
+			<hr />
+			<p></p>
+			<ExUseEffect01 />
+			<hr />
+			<p></p>
+			<ExUseEffect02 />
+			<hr />
+			<p></p>
+			<ExUseEffect03 />
+			<hr />
+			<p></p>
+			<ExUseEffect04 />
+			<hr />
+			<p></p>
+			<ExUseEffect05 />
+			<hr />
+			<p></p>
+			<MyForm />
+		</>
+	);
+}
--- src/comp/Layout.jsx
+++ src/comp/Layout.jsx
@@ -7,10 +7,15 @@ function Layout() {
 			<nav>
 				<ul>
 					<li>
-						<Link to="/proj-0810/">Home</Link>
+						<Link to="/react-site/day04_20250812">
+							Day04 - 20250812
+						</Link>
 					</li>
 					<li>
-						<Link to="/proj-0810/login">Login</Link>
+						<Link to="/react-site/">Home</Link>
+					</li>
+					<li>
+						<Link to="/react-site/login">Login</Link>
 					</li>
 				</ul>
 			</nav>
